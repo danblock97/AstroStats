74c0598cf1fd1bf3eab2740d5e31fa6696520f08
danblock97 <generalgoldiez@gmail.com>
2023-03-29 09:52:46 +0100

    Converted all commands to slash commands

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import datetime
 import discord
 import os
-import json
 from discord.ext import commands
 import requests
 from dotenv import load_dotenv
 from riotwatcher import LolWatcher
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = commands.Bot(command_prefix = "!", help_command=None, intents=intents) 
+client = commands.Bot(command_prefix = "!", help_command=None, intents=discord.Intents.all())
 
 load_dotenv()
 
@@ -24,74 +21,75 @@ async def on_ready():
         print(f"Commands Synced")
     except Exception as e:
         print(e)
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="NexusBot"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="NexusBot")) 
 
-#!help
-@client.command(pass_context=True,name="help")
-async def help(ctx):
+#/help
+@client.tree.command(name="help", description="Lists all available commands")
+async def help(interaction: discord.Interaction):
     embed=discord.Embed(title="NexusBot", color=0x1364a1)
-    embed.add_field(name="CSGO Lifetime Stats", value="`!csgo`")
-    embed.add_field(name="Apex Legends Lifetime Stats", value="`!apex`")
+    embed.add_field(name="CSGO Lifetime Stats", value="`/csgo`")
+    embed.add_field(name="Apex Legends Lifetime Stats", value="`/apex`")
+    embed.add_field(name="LoL Player Stats", value="`/lol`")
     embed.set_footer(text="Built By Goldiez" "\u2764\uFE0F")
-    await ctx.channel.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
-#!csgo
-@client.command()
-async def csgo(ctx, user_identifier=None):
-    if user_identifier is None:
-        await ctx.send("`!csgo <steamid_64>`")
+#/csgo
+@client.tree.command(name="csgo", description="Check your CSGO Lifetime Stats")
+async def csgo(interaction: discord.Interaction, name: str = None):
+    if name is None:
+        await interaction.response.send_message("`!csgo <steamid_64>`")
         return
 
     # Make the API request
-    response = requests.get(os.getenv('CSGO_API_URL').format(user_identifier), headers={"TRN-Api-Key": os.getenv('API_KEY')})
+    response = requests.get(os.getenv('CSGO_API_URL').format(name), headers={"TRN-Api-Key": os.getenv('API_KEY')})
 
     if response.status_code == 200:
         data = response.json()
         segments = data['data']['segments'][0]
         stats = segments['stats']
 
-        embed = discord.Embed(title=f"CS:GO - Lifetime Overview", url=os.getenv('CSGO_PROFILE_URL').format(user_identifier), color=0x1364a1)
+        embed = discord.Embed(title=f"CS:GO - Lifetime Overview", url=os.getenv('CSGO_PROFILE_URL').format(name), color=0x1364a1)
         for key, value in stats.items():
             print(f'{client.user} has retrieved your CSGO stats!')
             if isinstance(stats, dict):
                     embed.add_field(name=value['displayName'], value=value['displayValue'], inline=True)
                     embed.set_footer(text="Built By Goldiez" "\u2764\uFE0F")
-        await ctx.channel.send(embed=embed) 
+        await interaction.response.send_message(embed=embed) 
     else:
-        await ctx.send("`!csgo <steamid_64>`")
+        await interaction.response.send_message("`!csgo <steamid_64>`")
 
 #!apex
-@client.command()
-async def apex(ctx, user_identifier=None, platform=None):
-    if user_identifier is None:
-        await ctx.send("`!apex <steamid_64>`")
+@client.tree.command(name="apex", description="Check your Apex Lifetime Stats")
+async def apex(interation: discord.Interaction, name: str = None, platform: str = None ):
+    if name is None:
+        await interation.response.send_message("`!apex <steamid_64>`")
         if platform is None:
-         await ctx.send("`!apex <platform>`")
+         await interation.response.send_message("`!apex <platform>`")
         return
 
     # Make the API request
-    response = requests.get(os.getenv('APEX_API_URL').format(platform, user_identifier), headers={"TRN-Api-Key": os.getenv('API_KEY')}) 
+    response = requests.get(os.getenv('APEX_API_URL').format(platform, name), headers={"TRN-Api-Key": os.getenv('API_KEY')}) 
 
     if response.status_code == 200:
         data = response.json()
         segments = data['data']['segments'][0]
         stats = segments['stats']
 
-        embed = discord.Embed(title=f"Apex Legends - Lifetime Overview", url=os.getenv('APEX_PROFILE_URL').format(platform, user_identifier), color=0x1364a1)
+        embed = discord.Embed(title=f"Apex Legends - Lifetime Overview", url=os.getenv('APEX_PROFILE_URL').format(platform, name), color=0x1364a1)
         for key, value in stats.items():
             print(f'{client.user} has retrieved your Apex stats!')
             if isinstance(stats, dict):
                     embed.add_field(name=value['displayName'], value=value['displayValue'], inline=True)
                     embed.timestamp = datetime.datetime.utcnow()
                     embed.set_footer(text="Built By Goldiez" "\u2764\uFE0F")
-        await ctx.channel.send(embed=embed) 
+        await interation.response.send_message(embed=embed) 
     else:
-        await ctx.send("`!apex <steamid_64>`")
+        await interation.response.send_message("`!apex <steamid_64>`")
 
 #!lol
-@client.command(aliases=['lolstats'])
-async def lol(ctx, *,summonerName):
-    summoner = watcher.summoner.by_name('euw1',summonerName)
+@client.tree.command(name="lol", description="Check your LoL Player Stats")
+async def lol(interactions: discord.Interaction, *,summoner: str):
+    summoner = watcher.summoner.by_name('euw1',summoner)
     stats = watcher.league.by_summoner('euw1', summoner['id'])
     num = 0
     if (stats[0]['queueType'] == 'RANKED_SOLO_5x5'):
@@ -106,19 +104,19 @@ async def lol(ctx, *,summonerName):
     wr = int((wins/(wins+losses))* 100)
     hotStreak = int(stats[num]['hotStreak'])
     level = int(summoner['summonerLevel'])
+    icon = int(summoner['profileIconId'])
     embed = discord.Embed(title=f"League of Legends - Player Stats", color=0x1364a1)
     print(f'{client.user} has retrieved your LoL stats!')
+    embed.set_thumbnail(url=f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/{icon}.jpg")
+    embed.add_field(name="Ranked Solo/Duo", value=f'{str(tier)} {str(rank)} {str(lp)} LP \n Winrate: {str(wr)}% \n Winstreak: {str(hotStreak)}')
     embed.add_field(name="Level", value=f'{str(level)}')
-    embed.add_field(name="Rank", value=f'{str(tier)} {str(rank)} {str(lp)} LP')
-    embed.add_field(name="Winrate", value=f'{str(wr)}%')
-    embed.add_field(name="Hot Streak", value=f'{str(hotStreak)}')
     embed.timestamp = datetime.datetime.utcnow()
     embed.set_footer(text="Built By Goldiez" "\u2764\uFE0F")
-    await ctx.channel.send(embed=embed) 
+    await interactions.response.send_message(embed=embed) 
 
 @client.event
-async def p_error(ctx, error):
+async def p_error(interactions: discord.Interaction, error):
     if isinstance(error,commands.MissingRequiredArguments):
-        await ctx.send('Please specify a summoner name')
+        await interactions.response.send_message('Please specify a summoner name')
 
 client.run(os.getenv('TOKEN'))
