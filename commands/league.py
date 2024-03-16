35cefe0c675cd7eea8f089cec04ac664e9aa1f1c
danblock97 <generalgoldiez@gmail.com>
2024-03-16 21:33:16 +0000

    Rewritten League command to use new RiotID

--- commands/league.py
+++ commands/league.py
@@ -1,67 +1,78 @@
 import discord
 import datetime
 import requests
-from riotwatcher import LolWatcher
 from typing import Literal
 import os
 
-Region = Literal['Brazil', 'Europe Nordic & East', 'Europe', 'Japan', 'Korea', 'Latin America North',
-                 'Latin America South', 'North America', 'Oceania', 'Turkey', 'Russia', 'Philippines', 'Singapore']
+REGION_TO_PLATFORM = {
+    "EUROPE": ["EUW1", "EUN1", "TR1", "RU"],
+    "AMERICAS": ["BR1", "LA1", "LA2", "NA1"],
+    "ASIA": ["JP1", "KR", "SG2", "TH2", "TW2", "VN2"]
+}
 
-
-async def league(interaction: discord.Interaction, region: Region, *, summoner: str):
-    # Define a dictionary to map display names to Riot API region codes
-    region_mapping = {
-        "Brazil": "br1",
-        "Europe Nordic & East": "eun1",
-        "Europe": "euw1",
-        "Japan": "jp1",
-        "Korea": "kr",
-        "Latin America North": "la1",
-        "Latin America South": "la2",
-        "North America": "na1",
-        "Oceania": "oc1",
-        "Turkey": "tr1",
-        "Russia": "ru",
-        "Philippines": "ph2",
-        "Singapore": "sg2",
-    }
-
-    # Check if the provided region is valid
-    if region not in region_mapping:
-        await interaction.response.send_message("Invalid region. Please use a valid region name.")
+async def league(interaction: discord.Interaction, region: Literal['EUROPE', 'AMERICAS', 'ASIA'], *, summoner: str):
+    if region not in REGION_TO_PLATFORM:
+        await interaction.response.send_message("Invalid region. Please use a valid regional routing value.")
         return
 
     try:
-        # Get the Riot API region code from the dictionary
-        riot_region = region_mapping[region]
+        platform_regions = REGION_TO_PLATFORM[region]
 
-        lolWatcher = LolWatcher(os.getenv('RIOT_API'))
-        summoner = lolWatcher.summoner.by_name(riot_region, summoner)
-        stats = lolWatcher.league.by_summoner(riot_region, summoner['id'])
+        gameName, tagLine = summoner.split("#")
 
-        num = 0 if stats and stats[0]['queueType'] == 'RANKED_SOLO_5x5' else 1
+        riot_api_key = os.getenv('RIOT_API')
 
-        if not stats or not stats[num]:
-            raise ValueError("Invalid data structure in API response.")
+        headers = {
+            'X-Riot-Token': riot_api_key
+        }
 
-        tier = stats[num]['tier']
-        rank = stats[num]['rank']
-        lp = stats[num]['leaguePoints']
-        wins = int(stats[num]['wins'])
-        losses = int(stats[num]['losses'])
-        wr = int((wins / (wins + losses)) * 100)
-        hotStreak = int(stats[num]['hotStreak'])
-        level = int(summoner['summonerLevel'])
-        icon = int(summoner['profileIconId'])
+        puuid = None
+        for platform_region in platform_regions:
+            regional_url = f"https://{region.lower()}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
+            response = requests.get(regional_url, headers=headers)
+            if response.status_code == 200:
+                puuid = response.json().get('puuid')
+                break
+        if puuid is None:
+            await interaction.response.send_message("Failed to retrieve summoner data. The summoner may not exist or the service may be currently unavailable.")
+            return
+
+        summoner_response = requests.get(f"https://{platform_regions[0].lower()}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}", headers=headers)
+        summoner_response.raise_for_status()
+        summoner_data = summoner_response.json()
+
+        profile_icon_id = summoner_data['profileIconId']
+        summoner_level = summoner_data['summonerLevel']
+
+        league_response = requests.get(f"https://{platform_regions[0].lower()}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_data['id']}", headers=headers)
+        league_response.raise_for_status()
+        stats = league_response.json()
+
+        league_data_list = []
+        for league_data in stats:
+            queue_type = league_data['queueType']
+            tier = league_data['tier']
+            rank = league_data['rank']
+            lp = league_data['leaguePoints']
+            wins = int(league_data['wins'])
+            losses = int(league_data['losses'])
+            wr = int((wins / (wins + losses)) * 100)
+            league_data_list.append((queue_type, f"{tier} {rank} {lp} LP \n Win/Loss: {wins}W/{losses}L \n Winrate: {wr}%"))
 
         embed = discord.Embed(
-            title=f"League of Legends - Player Stats", color=0xdd4f7a)
+            title=f"{gameName}#{tagLine} - Level {summoner_level}", color=0xdd4f7a)
+
         embed.set_thumbnail(
-            url=f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/{icon}.jpg")
-        embed.add_field(name="Ranked Solo/Duo",
-                        value=f'{str(tier)} {str(rank)} {str(lp)} LP \n Winrate: {str(wr)}% \n Winstreak: {str(hotStreak)}')
-        embed.add_field(name="Level", value=f'{str(level)}')
+            url=f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/{profile_icon_id}.jpg")
+
+        for queue_type, league_data_str in league_data_list:
+            if queue_type == "RANKED_SOLO_5x5":
+                embed.add_field(name=f"Ranked Solo/Duo",
+                                value=league_data_str)
+            elif queue_type == "RANKED_FLEX_SR":
+                embed.add_field(name=f"Ranked Flex",
+                                value=league_data_str, inline=True)
+
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_footer(text="Built By Goldiez ❤️")
 
@@ -78,7 +89,6 @@ async def league(interaction: discord.Interaction, region: Region, *, summoner: 
     except Exception as e:
         print(f"Error: {e}")
         await interaction.response.send_message("Oops! An unexpected error occurred while processing your request. Please try again later.")
-
 
 def setup(client):
     client.tree.command(
