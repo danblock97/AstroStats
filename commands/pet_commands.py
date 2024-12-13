3bb8b60ab33c0055142e3adca34bef90cb12c6b3
danblock97 <generalgoldiez@gmail.com>
2024-12-13 07:53:43 +0000

    Moved the 'Support Us' field out of the for loop

--- commands/pet_commands.py
+++ commands/pet_commands.py
@@ -701,10 +701,10 @@ async def top_pets(interaction: discord.Interaction):
     for index, pet in enumerate(top_pets, 1):
         user = await client.fetch_user(int(pet['user_id']))
         embed.add_field(name=f"#{index}: {user.display_name}", value=f"Level: {pet['level']}, XP: {pet['xp']}", inline=False)
-        embed.add_field(name="Support Us ❤️",
-                        value="[If you enjoy using this bot, consider supporting us!](https://buymeacoffee.com/danblock97)")
         embed.set_thumbnail(url=pet['icon'])
 
+    embed.add_field(name="Support Us ❤️",
+                    value="[If you enjoy using this bot, consider supporting us!](https://buymeacoffee.com/danblock97)")
     await interaction.response.send_message(embed=embed)
 
 # Function to reset daily quests at midnight UTC
