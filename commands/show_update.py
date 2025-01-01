14517360ac8a106faf3174d22f243453cc966489
danblock97 <generalgoldiez@gmail.com>
2025-01-01 12:47:17 +0000

    Refactor update message formatting for improved readability

--- commands/show_update.py
+++ commands/show_update.py
@@ -1,21 +1,17 @@
 import discord
 
-# Easily editable text for the latest updates
-LATEST_UPDATES = """
-- **Version 1.4.4**:
-  **Bug Fixes and Enhancements** 🛠️
-
-  - **Error Handling in Commands**:
-    - **All Commands**: All commands now have a better error handling system. If you encounter an error, the bot will now respond with an appropriate embed message.
-
-  - **UI Enhancements**:
-      - **Improved Readability**: Some commands have been updated to have a better readability and user experience.
-
-  - **Voting Reminder**:
-    Remember to use `/vote` every 12 hours on **TOP.GG** to gain XP for your pets!
-"""
-
-
+LATEST_UPDATES = (
+    "**Version 1.4.4**:\n"
+    "**Bug Fixes and Enhancements** 🛠️\n\n"
+    "- **Error Handling in Commands**:\n"
+    "  - **All Commands**: All commands now have a better error handling system. "
+    "If you encounter an error, the bot will now respond with an appropriate embed message.\n\n"
+    "- **UI Enhancements**:\n"
+    "  - **Improved Readability**: Some commands have been updated to have a better "
+    "readability and user experience.\n\n"
+    "- **Voting Reminder**:\n"
+    "  Remember to use `/vote` every 12 hours on **TOP.GG** to gain XP for your pets!"
+)
 
 async def show_update(interaction: discord.Interaction):
     embed = discord.Embed(
@@ -23,13 +19,19 @@ async def show_update(interaction: discord.Interaction):
         description=LATEST_UPDATES,
         color=discord.Color.blue()
     )
-    embed.add_field(name="Support Us ❤️",
-                    value="[If you enjoy using this bot, consider supporting us!](https://buymeacoffee.com/danblock97)")
-    embed.set_footer(text=f"Requested by {interaction.user.display_name}", icon_url=interaction.user.avatar.url)
+    embed.add_field(
+        name="Support Us ❤️",
+        value=(
+            "[If you enjoy using this bot, consider supporting us!]"
+            "(https://buymeacoffee.com/danblock97)"
+        )
+    )
+    embed.set_footer(
+        text=f"Requested by {interaction.user.display_name}",
+        icon_url=interaction.user.avatar.url
+    )
     await interaction.response.send_message(embed=embed)
 
-
-# Setup function for the bot
 async def setup(client: discord.Client):
     client.tree.add_command(
         discord.app_commands.Command(
