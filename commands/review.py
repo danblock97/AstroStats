b74c4a90427dd12ec18c5e332203a63955c2e4a9
danblock97 <generalgoldiez@gmail.com>
2025-01-01 12:44:04 +0000

    Refactor review command for improved readability and structure

--- commands/review.py
+++ commands/review.py
@@ -2,28 +2,30 @@ import discord
 from discord import app_commands
 
 async def review(interaction: discord.Interaction):
-    
-    # Create an embed object
     embed = discord.Embed(
         title="Enjoying AstroStats?",
-        description="If you're enjoying AstroStats, please consider leaving a review on Top.gg!",
-        color=discord.Color.blue()  # You can change the color if you prefer
+        description=(
+            "If you're enjoying AstroStats, please consider leaving a review on Top.gg!"
+        ),
+        color=discord.Color.blue()
     )
-    
-    # Add a field with the review link
     embed.add_field(
         name="Leave a Review",
-        value="[Click here to leave a review on Top.gg](https://top.gg/bot/1088929834748616785#reviews)",
+        value=(
+            "[Click here to leave a review on Top.gg]"
+            "(https://top.gg/bot/1088929834748616785#reviews)"
+        ),
         inline=False
     )
-
-    embed.add_field(name="Support Us ❤️",
-                    value="[If you enjoy using this bot, consider supporting us!](https://buymeacoffee.com/danblock97)")
-
-    # Send the embed
+    embed.add_field(
+        name="Support Us ❤️",
+        value=(
+            "[If you enjoy using this bot, consider supporting us!]"
+            "(https://buymeacoffee.com/danblock97)"
+        )
+    )
     await interaction.response.send_message(embed=embed)
 
-# Setup function for the bot
 async def setup(client: discord.Client):
     client.tree.add_command(
         discord.app_commands.Command(
